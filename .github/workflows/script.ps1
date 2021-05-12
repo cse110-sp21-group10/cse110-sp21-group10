7d44195ce19950a13d0cb611b4e2520ff3ee9872
Akar Singh <aks002@ucsd.edu>
2021-05-11 21:32:20 -0700

    Fixed linter issues

--- .github/workflows/script.ps1
+++ .github/workflows/script.ps1
@@ -1,9 +1,9 @@
-# Define base variables 
+# Define base variables
 $strOrgName     = 'cse110-sp21-group10';
 $strRepoName    = $strOrgName;
 $strBaseURL     = ('https://api.github.com/repos/{0}/{1}' -f $strOrgName, $strRepoName);
 
-# Generate credentials from username and token 
+# Generate credentials from username and token
 $strCred        = ('{0}:{1}' -f 'maniacalhamster', $env:PERSONAL_TOKEN);
 $strAuthEncrypt = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($strCred));
 $strAuthBody    = ('Basic {0}' -f $strAuthEncrypt);
@@ -14,29 +14,29 @@ $numReqApprovals = 3;
 
 # Define helper function for making requests
 function makeRequest($strURL) {
-    Write-Host "Making call to: $strURL";
+    Write-Information "Making call to: $strURL";
 
     Invoke-RestMethod -Headers $objHeader -Uri $strURL;
 }
 
 # Define helper function to get open pull requests
 function getPulls() {
-    Write-Host "`t - Retreiving pull requests"
+    Write-Information "`t - Retreiving pull requests"
 
     $strPullsURL = "$strBaseURL/pulls";
 
-    Invoke-RestMethod -Method Put -Headers $objHeader -Uri $strMergeURL;
+    makeRequest $strPullsURL;
 }
 
 # Define a helper function to figure what label to apply based off user
-# - Uses a hashmap (group -> members, label) stored in a json file 
+# - Uses a hashmap (group -> members, label) stored in a json file
 function determineLabel($strUsername) {
     if (!$objTeams) {
-        Write-Host 'Reading team info from file...';
+        Write-Information 'Reading team info from file...';
         Set-Variable -Name objTeams -Scope Global -Value $(Get-Content 'teams.json' | ConvertFrom-Json);
     }
 
-    (Get-Member -InputObject $objTeams -MemberType NoteProperty).Name | ForEach {
+    (Get-Member -InputObject $objTeams -MemberType NoteProperty).Name | ForEach-Object {
         if ($objTeams.$_.members -contains $strUsername) {
             return $objTeams.$_.label;
         }
@@ -45,7 +45,7 @@ function determineLabel($strUsername) {
 
 # Define a helper function to apply label to a PR (via issue url)
 function applyLabel($strIssueURL, $numPullReqNum, $strLabel){
-    Write-Host "`t - Applying label <$strLabel> to PR #$numPullReqNum";
+    Write-Information "`t - Applying label <$strLabel> to PR #$numPullReqNum";
 
     $strLabelURL = "$strIssueURL/labels";
     $objBody = ConvertTo-Json @($strLabel);
@@ -53,9 +53,9 @@ function applyLabel($strIssueURL, $numPullReqNum, $strLabel){
     Invoke-RestMethod -Method Post -Body $objBody -Headers $objHeader -Uri $strLabelURL;
 }
 
-# Define a helper function to merge pull requests 
+# Define a helper function to merge pull requests
 function mergePull($strPullReqURL, $numPullReqNum) {
-    Write-Host "`t - Merging pull request #$numPullReqNum";
+    Write-Information "`t - Merging pull request #$numPullReqNum";
     $strMergeURL = "$strPullReqURL/merge";
 
     Invoke-RestMethod -Method Put -Headers $objHeader -Uri $strMergeURL;
@@ -69,17 +69,17 @@ function mergePull($strPullReqURL, $numPullReqNum) {
 #   - Finally check if PR has enough appprovals and merge
 $arrPullRequests = getPulls;
 
-$arrPullRequests | ForEach {
+$arrPullRequests | ForEach-Object {
     $strPullReqURL  = $_.url;
     $strIssueURL    = $_.issue_url;
     $numPullReqNum  = $_.number;
     $numApprovals   = 0;
     $arrReviews     = makeRequest "$strPullReqURL/reviews";
 
-    $arrReviews.State | ForEach {
+    $arrReviews.State | ForEach-Object {
         if ($_ -Match "Approved") {
             $strLabel   = determineLabel $_.user.login;
-            applyLabel $strIssueURL $numPullReqNum $strLabel;
+            applyLabel -strIssueURL $strIssueURL -numPullReqNum $numPullReqNum -strLabel $strLabel;
             $numApprovals++;
         }
     }
