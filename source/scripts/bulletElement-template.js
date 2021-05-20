d85978875ce89ed725f5f80898ab3a7be4dc4633
Ivan Ozerets <iozeres@ucsd.edu>
2021-05-20 14:56:56 -0700

    update 3:00pm 05/20

--- source/scripts/bulletElement-template.js
+++ source/scripts/bulletElement-template.js
@@ -57,10 +57,10 @@ class bulletElement extends HTMLElement {
             // </style>
 
           <div class="outer-bullet">
-            <span class="inner-bullet">
+            <div class="inner-bullet">
               <button class="bullet-button"><i class="fas fa-circle"></i></button>
               <p class="bullet-entry" contenteditable="true"></p>
-            </span>
+            </div>
           </div>
           `;
   
@@ -70,52 +70,10 @@ class bulletElement extends HTMLElement {
   
     get entry() {
 
-
-
-
-    //   let entryObj = {
-    //     'title': this.shadowRoot.querySelector('.entry-title').innerText,
-    //     'date': this.shadowRoot.querySelector('.entry-date').innerText,
-    //     'content': this.shadowRoot.querySelector('.entry-content').innerText,
-    //   };
-  
-    //   if (this.shadowRoot.querySelector('.entry-image')) {
-    //     let imageObj = {
-    //       'src': this.shadowRoot.querySelector('.entry-image').getAttribute('src'),
-    //       'alt': this.shadowRoot.querySelector('.entry-image').getAttribute('alt')
-    //     }
-    //     entryObj.image = imageObj;
-    //   }
-  
-    //   if (this.shadowRoot.querySelector('.entry-audio')) {
-    //     entryObj.audio = this.shadowRoot.querySelector('.entry-audio').getAttribute('src');
-    //   }
-  
-    //   return entryObj;
     }
   
     set entry(entry) {
 
-
-
-
-    //   this.shadowRoot.querySelector('.entry-title').innerText = entry.title;
-    //   this.shadowRoot.querySelector('.entry-date').innerText = entry.date;
-    //   this.shadowRoot.querySelector('.entry-content').innerText = entry.content;
-    //   if (entry.image) {
-    //     let entryImage = document.createElement('img');
-    //     entryImage.classList.add('entry-image');
-    //     entryImage.src = entry.image.src;
-    //     entryImage.alt = entry.image.alt;
-    //     this.shadowRoot.querySelector('.post').appendChild(entryImage);
-    //   }
-    //   if (entry.audio) {
-    //     let entryAudio = document.createElement('audio');
-    //     entryAudio.classList.add('entry-audio');
-    //     entryAudio.src = entry.audio;
-    //     entryAudio.controls = true;
-    //     this.shadowRoot.querySelector('.entry-audio-section').appendChild(entryAudio);
-    //   }
     }
   
   }
