72c8e80ba3e021814d853d63c033348157e90be8
Asya <akbalas@ucsd.edu>
2021-05-20 21:29:54 -0700

    bullet element css and html update

--- source/scripts/bulletElement-template.js
+++ source/scripts/bulletElement-template.js
@@ -6,62 +6,40 @@ class bulletElement extends HTMLElement {
   
       template.innerHTML = `
           <style>
-            //   .post {
-            //       max-width: 90vw;
-            //       padding: 20px;
-  
-            //       align-self: center;
-  
-            //       display: grid;
-            //       grid-template-areas: 
-            //         "title img img"
-            //         "content content content"
-            //         "content content content"
-            //         "audio audio audio";
-            //       gap: 10px;
-            //       grid-template-columns: repeat(2, 1fr);
-            //   }
-  
-            //   .entry-image {
-            //       grid-area: img
-            //       height: 100%;
-            //       align-self: center;
-            //       justify-self: center;
-            //       max-height: 350px;
-            //       max-width: 500px;
-            //       border-radius: 6px;
-            //   }
-  
-            //   .post section {
-            //     display: flex;
-            //     padding: 30px;
-            //     flex-direction: column;
-            //     border-radius: 6px;
-            //     background-color: white;
-            //     list-style-type: none;
-            //   }
-  
-            //   .entry-title-section {             
-            //       grid-area: title;
-            //   }
-  
-            //   .entry-content-section {
-            //       grid-area: content;
-            //   }
-  
-            //   .entry-audio-section {
-            //     grid-area: audio;
-            // }
-  
-  
-            // </style>
-
-          <div class="outer-bullet">
-            <div class="inner-bullet">
-              <button class="bullet-button"><i class="fas fa-circle"></i></button>
-              <p class="bullet-entry" contenteditable="true"></p>
+            .bullet-button {
+              font-size:10px;
+              border: none;
+              background: none;
+              float: left;
+              padding-top: 7px;
+            }
+            
+            .bullet-button:hover {
+              font-size: 12px;
+              position: relative;
+              right: 1px;
+              bottom: 1px;
+              width: 22px;
+            }
+            
+            .bullet-entry {
+              display: inline-block;
+              width: 80%;
+              font-size: larger;
+              margin: 0;
+              padding-left: 0.5em;
+            }
+            
+            [contenteditable] {
+              outline: 0px solid transparent;
+            }
+          
+            <div class="layer-1">
+              <div class="layer-2">
+                <button class="bullet-button"><i class="fas fa-circle"></i></button>
+                <p class="bullet-entry" contenteditable="true">holdonmmmmmm </p>
+              </div>
             </div>
-          </div>
           `;
   
       this.attachShadow({ mode: 'open' })
@@ -98,14 +76,3 @@ class bulletElement extends HTMLElement {
    *    }
    * }
    */
-
-  //Scratch code
-
-  // .bullet-button {
-  //   font-size:10px;
-  //   padding: 0;
-  //   border: none;
-  //   background: none;
-  // }
-
-  // <button class="bullet-button"><i class="fas fa-circle"></i></button>
