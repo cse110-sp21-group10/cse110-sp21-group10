84634c6a219b2197c6940a0c5c7c8a946e135cc8
Ivan  Ozerets <iozerets@ucsd.edu>
2021-05-28 12:29:47 -0700

    more syling changes

--- source/classes/bullet.js
+++ source/classes/bullet.js
@@ -49,7 +49,7 @@ class BulletEntry extends HTMLElement {
           border: none;
           background: none;
           float: left;
-          padding-top: .4%;
+          padding-top: .6%;
         }
 
         div < div.bullet-remove {
@@ -59,7 +59,7 @@ class BulletEntry extends HTMLElement {
         .bullet-point:hover {
           font-size: 12px;
           position: relative;
-          padding-top: .35%;
+          padding-top: .5%;
           right: 1px;
           width: 22px;
         }
@@ -78,7 +78,7 @@ class BulletEntry extends HTMLElement {
           margin-left: 1.8%;
 
           padding-left: .5%;
-          padding-top: .5%;
+          // padding-top: .5%;
 
           background-color: white;
         }
@@ -106,18 +106,18 @@ class BulletEntry extends HTMLElement {
         .bullet-remove:hover {
           font-size: 15px;
           position: relative;
-          padding-top: .1%;
+          padding-top: .35%;
         }
 
         .child-add:hover {
           font-size: 15px;
           position: relative;
-          padding-top: .1%;
+          padding-top: .35%;
         }
 
         .child-add,
         .bullet-remove {
-          padding-top: .2%;
+          padding-top: .4%;
 
           color: transparent;
         
