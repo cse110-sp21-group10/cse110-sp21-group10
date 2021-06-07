9211c69dfcc93c92b199df50027be444b6c47c4e
Akar Singh <aks002@ucsd.edu>
2021-06-07 16:53:30 -0700

    Auto fixed linter issues

--- tests/spa.test.js
+++ tests/spa.test.js
@@ -1,13 +1,13 @@
-import { IDConverter } from '../source/classes/IDConverter.js'
+import { IDConverter } from '../source/classes/IDConverter.js';
 
 // gets the current date and deconstructs it
-let currDate = new Date();
-let day = currDate.getDate();;
-let year = currDate.getFullYear();
+const currDate = new Date();
+const day = currDate.getDate(); ;
+const year = currDate.getFullYear();
 
-let currDay = IDConverter.getDayFromDate(currDate);
-let currMonth = IDConverter.getMonthFromDate(currDate);
-let currSuffix = IDConverter.getSuffixOfDate(currDate);
+const currDay = IDConverter.getDayFromDate(currDate);
+const currMonth = IDConverter.getMonthFromDate(currDate);
+const currSuffix = IDConverter.getSuffixOfDate(currDate);
 
 describe('Basic user flow for SPA ', () => {
   beforeAll(async () => {
@@ -19,15 +19,15 @@ describe('Basic user flow for SPA ', () => {
     const header = await page.$eval('daily-log', (elem) => {
       return elem.shadowRoot.querySelector('h1').textContent;
     });
-    expect(header).toBe(currDay + ", " + currMonth + " " + day + currSuffix);
+    expect(header).toBe(currDay + ', ' + currMonth + ' ' + day + currSuffix);
   });
-  
+
   it('Test 2: Clicking zoom out button should change page url to /#month', async () => {
-    await page.$$eval('button', (buttons) => { 
+    await page.$$eval('button', (buttons) => {
       for (let i = 0; i < buttons.length; i++) {
-        if (buttons[i].id == 'zoom-out-button') {
+        if (buttons[i].id === 'zoom-out-button') {
           buttons[i].click();
-        }        
+        }
       }
     });
     expect(page.url().includes('#month')).toBe(true);
@@ -37,15 +37,15 @@ describe('Basic user flow for SPA ', () => {
     const header = await page.$eval('monthly-log', (elem) => {
       return elem.shadowRoot.querySelector('h1').textContent;
     });
-    expect(header).toBe(currMonth + " " + year);
+    expect(header).toBe(currMonth + ' ' + year);
   });
-  
+
   it('Test 4: Clicking zoom out button should change page url to /#year', async () => {
-    await page.$$eval('button', (buttons) => { 
+    await page.$$eval('button', (buttons) => {
       for (let i = 0; i < buttons.length; i++) {
-        if (buttons[i].id == 'zoom-out-button') {
+        if (buttons[i].id === 'zoom-out-button') {
           buttons[i].click();
-        }        
+        }
       }
     });
     expect(page.url().includes('#year')).toBe(true);
@@ -55,7 +55,7 @@ describe('Basic user flow for SPA ', () => {
     const header = await page.$eval('yearly-log', (elem) => {
       return elem.shadowRoot.querySelector('h1').textContent;
     });
-    expect(header).toBe("" + year);
+    expect(header).toBe('' + year);
   });
 
   it('Test 6: Clicking the back buttons should change page url to /#month', async () => {
@@ -101,77 +101,77 @@ describe('Basic user flow for SPA ', () => {
   });
 
   it('Test 13: Clicking the last-entry-forward button should put us on the current day', async () => {
-    await page.$$eval('button', (buttons) => { 
+    await page.$$eval('button', (buttons) => {
       for (let i = 0; i < buttons.length; i++) {
-        if (buttons[i].id == 'last-entry-forward') {
+        if (buttons[i].id === 'last-entry-forward') {
           buttons[i].click();
-        }        
+        }
       }
     });
     const header = await page.$eval('daily-log', (elem) => {
       return elem.shadowRoot.querySelector('h1').textContent;
     });
-    expect(header).toBe(currDay + ", " + currMonth + " " + day + currSuffix);
+    expect(header).toBe(currDay + ', ' + currMonth + ' ' + day + currSuffix);
   });
 
   it('Test 14: Clicking the next-day button should put us on the next day', async () => {
-    await page.$$eval('button', (buttons) => { 
+    await page.$$eval('button', (buttons) => {
       for (let i = 0; i < buttons.length; i++) {
-        if (buttons[i].id == 'next-day') {
+        if (buttons[i].id === 'next-day') {
           buttons[i].click();
-        }        
+        }
       }
     });
     const header = await page.$eval('daily-log', (elem) => {
       return elem.shadowRoot.querySelector('h1').textContent;
     });
-    let nextDate = new Date();
+    const nextDate = new Date();
     nextDate.setDate(currDate.getDate() + 1);
-    
-    let nextDay = nextDate.getDate();
 
-    let nextDayOfWeek = IDConverter.getDayFromDate(nextDate);
-    let nextMonth = IDConverter.getMonthFromDate(nextDate);
-    let nextSuffix = IDConverter.getSuffixOfDate(nextDate);
-    
-    expect(header).toBe(nextDayOfWeek + ", " + nextMonth + " " + nextDay + nextSuffix);
+    const nextDay = nextDate.getDate();
+
+    const nextDayOfWeek = IDConverter.getDayFromDate(nextDate);
+    const nextMonth = IDConverter.getMonthFromDate(nextDate);
+    const nextSuffix = IDConverter.getSuffixOfDate(nextDate);
+
+    expect(header).toBe(nextDayOfWeek + ', ' + nextMonth + ' ' + nextDay + nextSuffix);
   });
 
   it('Test 15: Clicking the last-entry-back button should put us on the current day', async () => {
-    await page.$$eval('button', (buttons) => { 
+    await page.$$eval('button', (buttons) => {
       for (let i = 0; i < buttons.length; i++) {
-        if (buttons[i].id == 'last-entry-back') {
+        if (buttons[i].id === 'last-entry-back') {
           buttons[i].click();
-        }        
+        }
       }
     });
     const header = await page.$eval('daily-log', (elem) => {
       return elem.shadowRoot.querySelector('h1').textContent;
     });
-    expect(header).toBe(currDay + ", " + currMonth + " " + day + currSuffix);
+    expect(header).toBe(currDay + ', ' + currMonth + ' ' + day + currSuffix);
   });
 
   it('Test 16: Clicking the prev-day button should put us on the next day', async () => {
-    await page.$$eval('button', (buttons) => { 
+    await page.$$eval('button', (buttons) => {
       for (let i = 0; i < buttons.length; i++) {
-        if (buttons[i].id == 'prev-day') {
+        if (buttons[i].id === 'prev-day') {
           buttons[i].click();
-        }        
+        }
       }
     });
     const header = await page.$eval('daily-log', (elem) => {
       return elem.shadowRoot.querySelector('h1').textContent;
     });
-    let prevDate = new Date();
+    const prevDate = new Date();
     prevDate.setDate(currDate.getDate() - 1);
-    
-    let prevDay = prevDate.getDate();
 
-    let prevDayOfWeek = IDConverter.getDayFromDate(prevDate);
-    let prevMonth = IDConverter.getMonthFromDate(prevDate);
-    let prevSuffix = IDConverter.getSuffixOfDate(prevDate);
-    
-    expect(header).toBe(prevDayOfWeek + ", " + prevMonth + " " + prevDay + prevSuffix);
+    const prevDay = prevDate.getDate();
+
+    const prevDayOfWeek = IDConverter.getDayFromDate(prevDate);
+    const prevMonth = IDConverter.getMonthFromDate(prevDate);
+    const prevSuffix = IDConverter.getSuffixOfDate(prevDate);
+
+    expect(header).toBe(prevDayOfWeek + ', ' + prevMonth + ' ' + prevDay + prevSuffix);
   });
 
   /**
@@ -179,7 +179,6 @@ describe('Basic user flow for SPA ', () => {
    *    Clicking on the weather icon changes the temperature value
    *    Clicking on menu icon sets the right class for the 'index' div
    *    Changing the font sets the style of the body to the right font
-   *    
+   *
    */
-
 });
